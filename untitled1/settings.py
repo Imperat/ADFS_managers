de08a871545175eceed9bbc22cde5d0289087753
Imperat <power_2007_pro@mail.ru>
2015-07-04 23:54:44 +0300

    While development team model

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -26,6 +26,10 @@ TEMPLATE_DEBUG = True
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 ALLOWED_HOSTS = []
 
 
--- untitled1/urls.py
+++ untitled1/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
