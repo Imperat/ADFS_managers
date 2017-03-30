faeed9f18f56cec1d015e545edfa4c87a6880e96
Michael Lelyakin <michael.lelyakin@benchmarketing.com.au>
2017-03-30 18:29:59 +0400

    Start using webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+  context: __dirname,
+
+  entry: './static/js/main.js', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+  output: {
+      path: path.resolve('./static/bundles/'),
+      filename: "[name].js",
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015'],
+        }
+      }, // to transform JSX into JS
+    ],
+  },
+
+  resolve: {
+    modules: ['node_modules', 'bower_components'],
+    extensions: ['.js', '.jsx']
+  },
+}
