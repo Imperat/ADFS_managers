b738ddca52019cccaeef413f176539b8fadbcb82
Michael Lelyakin <mlelyakin@mirantis.com>
2017-06-21 19:28:16 +0400

    Dev js (#23)
    * Update settings.py
    
    * Create Procfile
    
    * Update Procfile
    
    * Update Procfile
    
    * Update Procfile
    
    * Update Procfile
    
    * Update Procfile
    
    * Update Procfile

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
