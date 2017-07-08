b54fa2c84f985ff7b402f221f9500cfe5fb10a4a
Michael Lelyakin <mlelyakin@mirantis.com>
2017-07-08 16:10:48 +0400

    Add supporting Python3 version (#26)

--- manage.py
+++ manage.py
@@ -2,10 +2,14 @@
 import os
 import sys
 
+python_version = sys.version_info.major
 
-import sys  
-reload(sys)  
-sys.setdefaultencoding('utf-8')
+if python_version == 3:
+    import importlib
+    importlib.reload(sys)
+else:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "untitled1.settings")
