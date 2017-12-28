34079a6f6200d0f79d13c5f9d807e3da83a6c9b4
Michael <michael.lelyakin@benchmarketing.com.au>
2017-12-29 00:41:28 +0400

    stadion reservation stage 2

--- static/js/stadionReservation.js
+++ static/js/stadionReservation.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import { Select } from 'antd';
-import { LocaleProvider } from 'antd';
-import { Button } from 'antd';
-import { TimePicker } from 'antd';
-import { DatePicker } from 'antd';
+import { Select, LocaleProvider, Button, TimePicker, DatePicker } from 'antd';
 import ruRu from 'antd/lib/locale-provider/ru_RU';
 
-const FontAwesome = require('react-fontawesome');
 const moment = require('moment');
 const Option = Select.Option;
 
@@ -22,9 +17,9 @@ export const renderStadionForm = () => {
 
       const startDate = moment().startOf('week');
       const endDate = moment().endOf('week');
-      const iterator = startDate.clone();
+
       const dates = {};
-      for (let i = startDate.date() + 1; i <= endDate.date(); i+= 1) {
+      for (let iterator = startDate.clone(); iterator.isSameOrBefore(endDate);) {
         dates[iterator.format('YYYY-MM-DD')] = [];
         iterator.add(1, 'day');
       }
@@ -36,8 +31,8 @@ export const renderStadionForm = () => {
         endDate,
         dates,
         selectedDate: null,
-        selectedTimeStart: '12:08',
-        selectedTimeEnd: '12:08',
+        selectedTimeStart: '12:00',
+        selectedTimeEnd: '13:00',
       };
 
       this.handleChangeStadion = this.handleChangeStadion.bind(this);
@@ -45,7 +40,27 @@ export const renderStadionForm = () => {
       this.handleChangeTimeStart = this.handleChangeTimeStart.bind(this);
       this.handleChangeTimeEnd = this.handleChangeTimeEnd.bind(this);
       this.handleTakeTime = this.handleTakeTime.bind(this);
+      this.handlePreviousWeek = this.handlePreviousWeek.bind(this);
+      this.handleNextWeek = this.handleNextWeek.bind(this);
       this.fetchStadions();
+    }
+
+    handlePreviousWeek() {
+      this.setState(prevState => Object.assign({}, this.state, {
+        startDate: this.state.startDate.clone().subtract(1, 'week'),
+        endDate: this.state.endDate.clone().subtract(1, 'week'),
+      }));
+
+      this.fillDates();
+    }
+
+    handleNextWeek() {
+      this.setState(prevState => Object.assign({}, this.state, {
+        startDate: this.state.startDate.clone().add(1, 'week'),
+        endDate: this.state.endDate.clone().add(1, 'week'),
+      }));
+
+      this.fillDates();
     }
 
     fetchStadions() {
@@ -85,6 +100,31 @@ export const renderStadionForm = () => {
       const selectedTimeEnd = this.state.selectedTimeEnd;
     }
 
+    stadionOptions() {
+      return this.state.stadions.map((stadion) => {
+        return <Option value={stadion.id}>{stadion.name}</Option>
+      });
+    }
+
+    timeBoardHeaders() {
+      return Object.keys(this.state.dates).map((date) => {
+        return <div className="col-lg-2 timeBoardHeaderItem">{date}</div>
+      });
+    }
+
+    fillDates() {
+      const dates = {};
+      const startDate = this.state.startDate;
+      const endDate = this.state.endDate;
+
+      for (let iterator = startDate.clone(); iterator.isSameOrBefore(endDate);) {
+        dates[iterator.format('YYYY-MM-DD')] = [];
+        iterator.add(1, 'day');
+      }
+
+      this.setState(prevState => Object.assign({}, prevState, { dates }));
+    }
+
     render () {
       return (
         <LocaleProvider locale={ruRu}>
@@ -101,28 +141,27 @@ export const renderStadionForm = () => {
               optionFilterProp="children"
               filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
             >
-            {this.state.stadions.map(function (i) {
-              return <Option value={i.id}>{i.name}</Option>
-            })}
+            {this.stadionOptions()}
             </Select>
-            <span className="panelLabel">Неделя: <a href="#">{this.state.startDate.format('MM-DD-YYYY')}</a> -
-                          <a href="#">{this.state.endDate.format('MM-DD-YYYY')}</a>
+            <span className="panelLabel">Неделя:
+              <a href="#">{this.state.startDate.format('DD-MM-YYYY')}</a> -
+              <a href="#">{this.state.endDate.format('DD-MM-YYYY')}</a>
+              <Button onClick={this.handlePreviousWeek}> - </Button>
+              <Button onClick={this.handleNextWeek}> + </Button>
             </span>
             <div className="take-time-panel" style={{ float: 'right' }}>
               Занять время:
               <DatePicker onChange={this.handleChangeNewDate} />
               <span>C:</span>
-              <TimePicker onChange={this.handleChangeTimeStart} defaultValue={moment('12:08', 'HH:mm')} format={format} />
+              <TimePicker onChange={this.handleChangeTimeStart} defaultValue={moment('12:10', 'HH:mm')} format={format} />
               <span>По:</span>
-              <TimePicker onChange={this.handleChangeTimeEnd} defaultValue={moment('12:08', 'HH:mm')} format={format} />
+              <TimePicker onChange={this.handleChangeTimeEnd} defaultValue={moment('13:10', 'HH:mm')} format={format} />
               <Button onClick={this.handleTakeTime} >OK</Button>
             </div>
           </div>
           <div className="col-lg-12">
-            <div className="row timeBoardHeader">
-              { Object.keys(this.state.dates).map(function(i){
-                return <div className="col-lg-2 timeBoardHeaderItem">{i}</div>
-              }) }
+            <div className="timeBoardHeader">
+              {this.timeBoardHeaders()}
             </div>
             <div className="row timeBoardBody">
               { Object.keys(this.state.dates).map((i) => {
